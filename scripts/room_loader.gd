659515d0e2abda72cfc83ee80ea4c29608a8e909
ScriptMage <scriptmage037@gmail.com>
2025-06-16 21:42:20 +0200

    Room loader

--- scripts/room_loader.gd
+++ scripts/room_loader.gd
@@ -2,14 +2,27 @@ extends Node2D
 
 class_name RoomLoader
 
-@export var roomToSpawns : Array(PackedScene)
+@export var firstRoom : PackedScene
+@export var roomToSpawns : Array[PackedScene] = []
+
+var _nextSpawnPoint : Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:	
-	var spawned = roomToSpawn.instantiate()
-	add_child(spawned)	
+func _ready() -> void:
+	spawnRoom(firstRoom, 0)
+	
+	for n in range(1, 10):
+		spawnRoom(roomToSpawns.pick_random(), n)
 	pass # Replace with function body.
 
+
+func spawnRoom(roomToSpawn: PackedScene, i: int) -> void:
+	var spawned : Room = roomToSpawn.instantiate()
+	spawned.name = spawned.name + "_" + str(i)
+	add_child(spawned)
+	spawned.global_position = _nextSpawnPoint
+	var exit : RoomExit = spawned.roomExit
+	_nextSpawnPoint = exit.global_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
