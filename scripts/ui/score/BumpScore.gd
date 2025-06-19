39900f714d4bf1cf4d89462fd00195b954f6e367
seehj <seeehj@gmail.com>
2025-06-19 13:44:40 +0200

    tweaked float text time

--- scripts/ui/score/BumpScore.gd
+++ scripts/ui/score/BumpScore.gd
@@ -3,6 +3,7 @@ class_name FloatingScore
 
 @export var Lifetime : float
 
+
 func _ready() -> void:
 	is_centered = true
 
@@ -11,6 +12,9 @@ func _process(_delta: float) -> void:
 
 func SetPos(pos:Vector2) -> void:
 	global_position = pos
+	scale = Vector2.ZERO
+	# rotation = frange(-0.2, 0.2).pick_random()
+	rotation = randf_range(-.3, .3)
 
 func set_number(number : int) -> void:
 	super(number)
@@ -18,6 +22,8 @@ func set_number(number : int) -> void:
 	Animate()
 
 func Animate() -> void:
-	var tween : Tween = get_tree().create_tween()
-	tween.tween_property(self, "position", Vector2(global_position.x, global_position.y*1.1), Lifetime)
-	tween.tween_callback(self.queue_free).set_delay(Lifetime)
+	var tween : Tween = get_tree().create_tween().set_ignore_time_scale(true)
+	tween.tween_property(self, "scale", Vector2.ONE, Lifetime).set_trans(Tween.TRANS_BOUNCE)
+	tween.tween_property(self, "scale", Vector2.ZERO, Lifetime).set_delay(Lifetime/2).set_trans(Tween.TRANS_CUBIC)
+
+	tween.tween_callback(self.queue_free).set_delay(Lifetime*1.5)
