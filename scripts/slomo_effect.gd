66edfc2bbe73b12ddb08c1bde120f74bd112f112
ScriptMage <scriptmage037@gmail.com>
2025-06-20 22:29:05 +0200

    curves be curving

--- scripts/slomo_effect.gd
+++ scripts/slomo_effect.gd
@@ -2,6 +2,7 @@ class_name SlomoEffect extends TextureRect
 @export var enabled: bool = true
 @export var abbarationCurve : Curve
 @export var abbarationPowerCurve : Curve
+@export var abbarationOffsetCurve : Curve
 
 func _ready() -> void:
 	Global.player_charge_duration_percent_changed.connect(charge_changed)
@@ -17,8 +18,11 @@ func charge_changed(newValue: float):
 	
 	var radius = abbarationCurve.sample(newValue)
 	var power = abbarationPowerCurve.sample(newValue)
+	var offset = abbarationOffsetCurve.sample(newValue)
 	shaderMat.set_shader_parameter("radius", radius)
 	shaderMat.set_shader_parameter("chromatic_intesity", power)
+	shaderMat.set_shader_parameter("chromatic_offset", power)
+	
 	if newValue > 0:
 		visible = true
 	else:
