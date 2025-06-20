25bfa9fc5b79aa2ce4f3d9af54cf35411d8ca5d8
seehj <seeehj@gmail.com>
2025-06-20 23:22:18 +0200

    Bullet animation

--- scripts/bullet_display.gd
+++ scripts/bullet_display.gd
@@ -1,13 +1,28 @@
 class_name BulletDisplay extends Sprite2D
 
+@export var animationSheet : Texture2D
+@export var animationFPS : int
+@export var animationTime : float
+
+var fps
+
 func fire():
-	queue_free()
+	Animate()
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func Animate():
+	texture = animationSheet
+	hframes = 8
 
+	var frames : Tween = get_tree().create_tween().set_loops(8)
+	frames.tween_callback(MoveAnimation).set_delay(0.1)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	var position : Tween = get_tree().create_tween()
+	position.tween_property(self, "position", self.position + Vector2(2, -1) * 80, animationTime).set_trans(Tween.TRANS_QUAD)
+	position.tween_callback(queue_free).set_delay(animationTime)
+
+var currentframe := 0
+
+func MoveAnimation():
+	currentframe += 1
+	frame = currentframe
+	frame_coords.x = currentframe
