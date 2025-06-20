4af16446ed6278febfd883dd0a8ede8caf64690c
ScriptMage <scriptmage037@gmail.com>
2025-06-20 14:19:18 +0200

    Properly grab focus

--- scripts/player_name_input.gd
+++ scripts/player_name_input.gd
@@ -4,6 +4,7 @@ extends LineEdit
 func _ready() -> void:
 	grab_focus()
 	set_caret_column(text.length())
+	Global.game_over.connect(grab_focus)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
